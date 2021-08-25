6460191adec11eba77af4aae52118bbcca18a178
Cawe Alves <84283988+Robertao541@users.noreply.github.com>
2021-08-25 14:19:03 -0300

    beta 0.1.0

--- Model/Recognition.py
+++ Model/Recognition.py
@@ -4,12 +4,17 @@ import gc
 
 class Recognition:
     def __new__(self, directory):  
-        self.position = pg.locateCenterOnScreen(directory)
+        self.position = pg.locateCenterOnScreen(directory, confidence=0.5)
         print(pg.locateCenterOnScreen(directory))          
         if self.position != None:
+            a = print("first condition")
+            resolution = pg.size() 
+            print(resolution)
             return Click(self.position)
-        else:   
+        else:
+            b = print("second condition")
             del self.position
+            del b
             gc.collect()
             return Recognition(directory)
         
