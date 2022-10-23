e8762acf1559e5fc5c9e28604184dc6b2d0dd37d
cawe541 <cawe.wewe04@gmail.com>
2022-10-22 22:14:17 -0300

    add base_url atributo

--- app/service/LeagueOfLegendsService.py
+++ app/service/LeagueOfLegendsService.py
@@ -1,18 +1,18 @@
 import requests
 
 class LeagueOfLegends:
-    def __init__(self):
-        pass
-
-    async def get_champion_list():
+    def __init__(self, base_url):
+        self.base_url = "http://ddragon.leagueoflegends.com/cdn/"
+    
+    async def get_champion_list(self):
         all_champions = []
 
-        response = await requests.get('http://ddragon.leagueoflegends.com/cdn/12.20.1/data/en_US/champion.json')
-        championsJson = response.json()
-        for champion in championsJson['data']:
+        response = await requests.get( self.base_url + '12.20.1/data/en_US/champion.json')
+        champions_json = response.json()
+        for champion in champions_json['data']:
             all_champions.append(champion)
         return all_champions
 
-    def get_champion_image(champion):
-        url = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + champion + ".png"
+    def get_champion_image(self, champion):
+        url = self.base_url + "6.24.1/img/champion/" + champion + ".png"
         return url
