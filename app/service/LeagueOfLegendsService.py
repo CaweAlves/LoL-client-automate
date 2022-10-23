6d0710523b7c13b7d2c0911bdf0c42ea7d0fb8a4
MTSKaioken <kaio.martins2@aluno.unip.br>
2022-10-22 21:46:25 -0300

    lista com todos campeões do lol

--- app/service/LeagueOfLegendsService.py
+++ app/service/LeagueOfLegendsService.py
@@ -1,3 +1,17 @@
+import requests
+
 class LeagueOfLegends:
     def __init__(self):
+        pass
+
+    async def get_champion_list():
+        all_champions = []
+
+        response = await requests.get('http://ddragon.leagueoflegends.com/cdn/12.20.1/data/en_US/champion.json')
+        championsJson = response.json()
+        for champion in championsJson['data']:
+            all_champions.append(champion)
+        return all_champions
+
+    def get_champion_image(champion):
         pass
